dd692cecee4d0ae4aea30c67f038805929439d66
gazorpazorp85 <paolo.groppi@gmail.com>
2020-02-19 21:05:55 +0200

    Updates

---
+++ precache-manifest.5232c103b9f241e2f61f36a97ad4c0c5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bca8c80baab003466e961de0253477",
+    "url": "/myteam-pixelperfect/index.html"
+  },
+  {
+    "revision": "73c2d40e801d918806f9",
+    "url": "/myteam-pixelperfect/static/css/main.b537cb0b.chunk.css"
+  },
+  {
+    "revision": "925e56783daffb4ce50b",
+    "url": "/myteam-pixelperfect/static/js/2.0d58fffd.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/myteam-pixelperfect/static/js/2.0d58fffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c2d40e801d918806f9",
+    "url": "/myteam-pixelperfect/static/js/main.4c337a45.chunk.js"
+  },
+  {
+    "revision": "0d33193dc7b4eed227fd",
+    "url": "/myteam-pixelperfect/static/js/runtime-main.6e3c862e.js"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/myteam-pixelperfect/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "9939ebf90a63343d97a1f13099d07068",
+    "url": "/myteam-pixelperfect/static/media/Livvic-MediumItalic.9939ebf9.ttf"
+  },
+  {
+    "revision": "686d9685b805c407876cd1abd698fd73",
+    "url": "/myteam-pixelperfect/static/media/Livvic-Regular.686d9685.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/myteam-pixelperfect/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "df4410aead66f0adfa80154f254dc562",
+    "url": "/myteam-pixelperfect/static/media/icon-hamburger.df4410ae.svg"
+  },
+  {
+    "revision": "22c2459ee9b2f81c568b15fdfa3e84bf",
+    "url": "/myteam-pixelperfect/static/media/logo.22c2459e.svg"
+  },
+  {
+    "revision": "fc4d3b2439e88c45670a3cad24310fd4",
+    "url": "/myteam-pixelperfect/static/media/oval-arthur.fc4d3b24.png"
+  },
+  {
+    "revision": "95b2112a021187996205069fadab6560",
+    "url": "/myteam-pixelperfect/static/media/oval-kady.95b2112a.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/myteam-pixelperfect/precache-manifest.5232c103b9f241e2f61f36a97ad4c0c5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myteam-pixelperfect/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
